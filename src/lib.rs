5f84d3a81739e5a48cf43c658dd5e1e70a03706c
Abhishek Chanda <abhishek.chanda@emc.com>
2016-01-09 18:02:19 +0530

    Use the ip crate

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,14 @@
 #![cfg_attr(feature = "dev", feature(plugin))]
 #![cfg_attr(feature = "dev", plugin(clippy))]
 #![crate_type = "lib"]
-#![feature(ip_addr)]
 #[allow(dead_code)]
 
+extern crate ip;
+
 use std::fmt;
-use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
+use std::net::{Ipv4Addr, Ipv6Addr};
+
+use ip::IpAddr;
 
 // A network
 #[derive(Debug)]
