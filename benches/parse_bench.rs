2b9936fa8e6a92fecfff8a5967e240f721fbdd8a
Abhishek Chanda <abhishek.becs@gmail.com>
2019-02-02 23:36:08 +0000

    Track benchmark for contains

--- benches/parse_bench.rs
+++ benches/parse_bench.rs
@@ -5,6 +5,8 @@ extern crate ipnetwork;
 use ipnetwork::{Ipv4Network, Ipv6Network};
 use criterion::Criterion;
 
+use std::net::{Ipv4Addr, Ipv6Addr};
+
 fn parse_ipv4_benchmark(c: &mut Criterion) {
     c.bench_function("parse ipv4", |b| b.iter(|| {
         "127.1.0.0/24".parse::<Ipv4Network>().unwrap()
@@ -17,5 +19,19 @@ fn parse_ipv6_benchmark(c: &mut Criterion) {
     }));
 }
 
-criterion_group!(benches, parse_ipv4_benchmark, parse_ipv6_benchmark);
+fn contains_ipv4_benchmark(c: &mut Criterion) {
+    c.bench_function("contains ipv4", |b| b.iter(|| {
+        let cidr = "74.125.227.0/25".parse::<Ipv4Network>().unwrap();
+        cidr.contains(Ipv4Addr::new(74, 125, 227, 4))
+    }));
+}
+
+fn contains_ipv6_benchmark(c: &mut Criterion) {
+    c.bench_function("contains ipv6", |b| b.iter(|| {
+        let cidr = "FF01:0:0:17:0:0:0:2/65".parse::<Ipv6Network>().unwrap();
+        cidr.contains(Ipv6Addr::new(0xff01, 0, 0, 0x17, 0x7fff, 0, 0, 0x2))
+    }));
+}
+
+criterion_group!(benches, parse_ipv4_benchmark, parse_ipv6_benchmark, contains_ipv4_benchmark, contains_ipv6_benchmark);
 criterion_main!(benches);
